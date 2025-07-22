b2f95c5cd815d2d79a0d32316815b14b2d71a328
Hervé Rouillard <rvcestmoi@hotmail.com>
2025-07-22 14:16:26 +0200

    V1 Windows Version

---
+++ taskmanager.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d C:\git\task_manager
+
+echo Lancement de Flask...
+start http://localhost:5050	
+
+C:\Users\hrouillard\AppData\Local\Programs\Python\Python311\python.exe app.py
+
+pause
